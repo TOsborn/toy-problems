f13817263349db6ffd04dac2155250950182fb3d
Trenton Osborn <os.trenton@gmail.com>
2019-10-09 15:31:46 -0700

    add problem 1 and replace hyphens with underscores in all file and directory names

---
+++ leetcode_weekly_contest_156/problem_1.py
@@ -0,0 +1,6 @@
+from collections import Counter
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        counts = Counter(arr)
+        return len(counts) == len(set(counts.values()))
