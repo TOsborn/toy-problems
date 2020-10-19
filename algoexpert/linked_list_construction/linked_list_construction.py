f634104128d72758402c8d923ed229f87d861c29
Trenton Osborn <os.trenton@gmail.com>
2020-10-18 20:29:45 -0700

    solve linked list construction

--- algoexpert/linked_list_construction/linked_list_construction.py
+++ algoexpert/linked_list_construction/linked_list_construction.py
@@ -1,67 +1,216 @@
 class Node:
+    """Node class for use in a doubly-linked list."""
     def __init__(self, value):
         self.value = value
         self.prev = None
         self.next = None
 
+    def __repr__(self):
+        prev_val = None if self.prev is None else self.prev.value
+        next_val = None if self.next is None else self.next.value
+        return f"({prev_val}<{self.value}>{next_val})"
+
+    def __eq__(self, other):
+        self_prev_val = None if self.prev is None else self.prev.value
+        self_next_val = None if self.next is None else self.next.value
+
+        other_prev_val = None if other.prev is None else other.prev.value
+        other_next_val = None if other.next is None else other.next.value
+
+        value_eq = (self.value == other.value)
+        prev_eq = (self_prev_val == other_prev_val)
+        next_eq = (self_next_val == other_next_val)
+
+        return value_eq and prev_eq and next_eq
+
+
 
 class DoublyLinkedList:
-    def __init__(self, from_list=None):
+    """Doubly-linked list class.
+    
+    Supports efficient insertion and removal of nodes.
+    """
+    def __init__(self, data=None):
         self.head = None
         self.tail = None
-
-        if from_list:
-            self.head = 
+        if data:
+            for value in data:
+                self.setTail(Node(value))
 
     def setHead(self, node):
-        node.next = self.head
-        self.head.prev = node
-        self.head = node
+        """Insert a node in the head position.
+
+        If the node already appears in the list, it is moved to the
+        head position.
+        """
+        if node in self:
+            self.remove(node)
+
+        if self.head:
+            node.next = self.head
+            self.head.prev = node
+            self.head = node
+        else:
+            self.head = self.tail = node
 
     def setTail(self, node):
-        self.tail.next = node
-        node.prev = self.tail
-        self.tail = node
+        """Insert a node in the tail position.
+
+        If the node already appears in the list, it is moved to the
+        tail position.
+        """
+        if node in self:
+            self.remove(node)
+
+        if self.tail:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+        else:
+            self.head = self.tail = node
 
     def insertBefore(self, node, nodeToInsert):
-        # link node.prev with nodeToInsert
-        if node.prev:
-            node.prev.next = nodeToInsert
-            nodeToInsert.prev = node.prev
+        """Insert a node immediately before a given node.
 
-        # link nodeToInsert with node
-        nodeToInsert.next = node
-        node.prev = nodeToInsert
+        If the node to insert already appears in the list, it is moved
+        to the new position.
+        """
+        if node is nodeToInsert:
+            return
+        
+        if nodeToInsert in self:
+            self.remove(nodeToInsert)
+        
+        if node is self.head:
+            self.setHead(nodeToInsert)
+        else:
+            # link node.prev with nodeToInsert
+            if node.prev:
+                node.prev.next = nodeToInsert
+                nodeToInsert.prev = node.prev
+
+            # link nodeToInsert with node
+            nodeToInsert.next = node
+            node.prev = nodeToInsert
 
     def insertAfter(self, node, nodeToInsert):
-        # link node with nodeToInsert
-        node.next = nodeToInsert
-        nodeToInsert.prev = node
+        """Insert a node immediately after a given nodee.
 
-        # link nodeToInsert with node.next
-        if node.next:
-            nodeToInsert.next = node.next
+        If the node to insert already appears in the list, it is moved
+        to the new position.
+        """
+        if node is nodeToInsert:
+            return
+
+        if nodeToInsert in self:
+            self.remove(nodeToInsert)
+        
+        if node is self.tail:
+            self.setTail(nodeToInsert)
+        else:
+            # link node with nodeToInsert
+            nxt = node.next
             node.next = nodeToInsert
+            nodeToInsert.prev = node
+
+            # link nodeToInsert with node.next
+            if nxt:
+                nodeToInsert.next = nxt
+                nxt.prev = nodeToInsert
 
     def insertAtPosition(self, position, nodeToInsert):
-        node = self.head
-        for _ in range(position):
-            node = node.next
+        """Insert a node at a given index.
+        
+        If the node to insert already appears in the list, it is moved
+        to the new position.
 
-        self.insertBefore(node, nodeToInsert)
+        Note: If the node already appears in the list, and in
+        particular if the node appears before the given insertion
+        point, this method inserts the node at what would be the given
+        position if the node were still at its original position. I 
+        view this functionality as incorrect and have included a 
+        separate method, insertAtPosition2, which always results in the
+        inserted node appearing at the given position.
+        """
+        if position == 1:
+            self.setHead(nodeToInsert)
+        else:
+            node = self.head
+            for _ in range(position-2):
+                node = node.next
+
+            if nodeToInsert in self:
+                if nodeToInsert is node:
+                    return
+                else:
+                    self.remove(nodeToInsert)
+
+            self.insertAfter(node, nodeToInsert)
+    
+    def insertAtPosition2(self, position, nodeToInsert):
+        """Insert a node at a given index.
+        
+        If the node to insert already appears in the list, it is moved
+        to the new position.
+
+        This method is an alternative to insertAtPosition which always
+        results in the node appearing at the given position in the
+        modified list.
+        """
+        if nodeToInsert in self:
+            self.remove(nodeToInsert)
+        
+        if position == 1:
+            self.setHead(nodeToInsert)
+        else:
+            node = self.head
+            for _ in range(position-2):
+                node = node.next
+
+            self.insertAfter(node, nodeToInsert)
 
     def removeNodesWithValue(self, value):
+        """Remove all nodes containing a given value."""
+        to_remove = []
         for node in self:
             if node.value == value:
-                self.remove(node)
+                to_remove.append(node)
+
+        for node in to_remove:
+            self.remove(node)
 
     def remove(self, node):
-        # Write your code here.
-        pass
+        """Remove a given node.
+        
+        Requires that the node be present in the list.
+        """
+        assert node in self
+        if node is self.head:
+            if self.head is self.tail:
+                self.head = self.tail = None
+            else:
+                self.head = node.next
+                self.head.prev = None
+        elif node is self.tail:
+            self.tail = self.tail.prev
+            self.tail.next = None
+        else:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+
+        node.prev = node.next = None
 
     def containsNodeWithValue(self, value):
-        # Write your code here.
-        pass
+        """Check whether list contains a node with a given value."""
+        for node in self:
+            if node.value == value:
+                return True
+        
+        return False
+
+    def to_list(self):
+        """Create a python type list of values."""
+        return [node.value for node in self]
 
     def __iter__(self):
         node = self.head
@@ -69,10 +218,21 @@ class DoublyLinkedList:
             yield node
             node = node.next
 
-    def to_list(self):
-        as_list = []
-        for node in self:
-            as_list.append(node)
+    def __contains__(self, node):
+        for node2 in self:
+            if node2 is node:
+                return True
 
-        return as_list
+        return False
 
+    def __len__(self):
+        return len(iter(self))
+
+    def __bool__(self):
+        return self.head is not None
+
+    def __repr__(self):
+        return repr(list(self))
+
+    def __eq__(self, other):
+        return list(self) == list(other)
--- algoexpert/linked_list_construction/test_linked_list_construction.py
+++ algoexpert/linked_list_construction/test_linked_list_construction.py
@@ -1,4 +1,345 @@
-import linked_list_construction
+from linked_list_construction import DoublyLinkedList, Node
 
-def test_sethead():
-    pass
+# constructor
+def test_constructor_multiple_nodes():
+    dll = DoublyLinkedList(range(3))
+
+    assert dll.head.value == 0
+    middle = dll.head.next
+    assert middle.value == 1
+    assert middle.prev == dll.head
+    assert middle.next == dll.tail
+    assert dll.tail.value == 2
+
+def test_constructor_empty():
+    dll = DoublyLinkedList()
+
+    assert dll.to_list() == []
+    assert dll.head is dll.tail is None
+
+def test_constructor_single_node():
+    dll = DoublyLinkedList([0])
+    
+    assert dll.head is dll.tail
+    assert dll.head.value == 0
+
+def test_constructor_two_nodes():
+    dll = DoublyLinkedList([0, 1])
+
+    assert dll.head.value == 0
+    assert dll.tail.value == 1
+
+    assert dll.head.next is dll.tail
+    assert dll.tail.prev is dll.head
+
+# setHead
+def test_setHead_on_empty():
+    dll = DoublyLinkedList()
+    dll.setHead(Node(0))
+
+    assert dll == DoublyLinkedList([0])
+
+def test_setHead_nonempty():
+    dll = DoublyLinkedList([0, 1])
+    dll.setHead(Node(-1))
+
+    assert dll == DoublyLinkedList([-1, 0, 1])
+
+# setTail
+def test_setTail_on_empty():
+    dll = DoublyLinkedList()
+    dll.setHead(Node(0))
+
+    assert dll == DoublyLinkedList([0])
+
+def test_setTail_nonempty():
+    dll = DoublyLinkedList([0, 1])
+    dll.setTail(Node(2))
+
+    assert dll == DoublyLinkedList([0, 1, 2])
+
+# insertBefore
+def test_insertBefore_single_node():
+    dll = DoublyLinkedList([0])
+    dll.insertBefore(dll.head, Node(-1))
+
+    assert dll == DoublyLinkedList([-1, 0])
+
+def test_insertBefore_head():
+    dll = DoublyLinkedList([0, 1, 2])
+    dll.insertBefore(dll.head, Node(-1))
+
+    assert dll == DoublyLinkedList([-1, 0, 1, 2])
+
+def test_insertBefore_node_after_head():
+    dll = DoublyLinkedList([0, 2, 3])
+    dll.insertBefore(dll.head.next, Node(1))
+
+    assert dll == DoublyLinkedList([0, 1, 2, 3])
+
+def test_insertBefore_tail():
+    dll = DoublyLinkedList([0, 1, 3])
+    dll.insertBefore(dll.tail, Node(2))
+
+    assert dll == DoublyLinkedList([0, 1, 2, 3])
+
+def test_insertBefore_middle():
+    dll = DoublyLinkedList([0, 2, 3])
+    dll.insertBefore(dll.head.next, Node(1))
+
+    assert dll == DoublyLinkedList([0, 1, 2, 3])
+
+def test_insertBefore_already_present():
+    dll = DoublyLinkedList([0, 1, 2, 3])
+
+    dll.insertBefore(dll.head, dll.head)
+    assert dll == DoublyLinkedList([0, 1, 2, 3])
+
+    dll.insertBefore(dll.head.next, dll.head)
+    assert dll == DoublyLinkedList([0, 1, 2, 3])
+
+    dll.insertBefore(dll.head.next.next, dll.head)
+    assert dll == DoublyLinkedList([1, 0, 2, 3])
+
+    dll = DoublyLinkedList([0, 1, 2, 3])
+    dll.insertBefore(dll.head, dll.tail)
+    assert dll == DoublyLinkedList([3, 0, 1, 2])
+
+# insertAfter
+def test_insertAfter_single_node():
+    dll = DoublyLinkedList([0])
+    dll.insertAfter(dll.head, Node(1))
+
+    assert dll == DoublyLinkedList([0, 1])
+
+def test_insertAfter_head():
+    dll = DoublyLinkedList([0, 2, 3])
+    dll.insertAfter(dll.head, Node(1))
+
+    assert dll == DoublyLinkedList([0, 1, 2, 3])
+
+def test_insertAfter_node_before_tail():
+    dll = DoublyLinkedList([0, 1, 3])
+    dll.insertAfter(dll.tail.prev, Node(2))
+
+    assert dll == DoublyLinkedList([0, 1, 2, 3])
+
+def test_insertAfter_tail():
+    dll = DoublyLinkedList([0, 1, 2])
+    dll.insertAfter(dll.tail, Node(3))
+
+    assert dll == DoublyLinkedList([0, 1, 2, 3])
+
+def test_insertAfter_middle():
+    dll = DoublyLinkedList([0, 1, 3])
+    dll.insertAfter(dll.head.next, Node(2))
+
+    assert dll == DoublyLinkedList([0, 1, 2, 3])
+
+def test_insertAfter_already_present():
+    dll = DoublyLinkedList([0, 1, 2, 3])
+
+    dll.insertAfter(dll.tail, dll.tail)
+    assert dll == DoublyLinkedList([0, 1, 2, 3])
+
+    dll.insertAfter(dll.tail.prev, dll.tail)
+    assert dll == DoublyLinkedList([0, 1, 2, 3])
+
+    dll.insertAfter(dll.head.next.next, dll.head.next)
+    assert dll == DoublyLinkedList([0, 2, 1, 3])
+
+# insertAtPosition
+def test_insertAtPosition2():
+    dll = DoublyLinkedList()
+    dll.insertAtPosition2(0, Node(0))
+
+    assert dll == DoublyLinkedList([0])
+
+    dll.insertAtPosition2(2, Node(1))
+    assert dll == DoublyLinkedList([0, 1])
+
+    dll.insertAtPosition2(1, Node(-1))
+    assert dll == DoublyLinkedList([-1, 0, 1])
+
+    dll.insertAtPosition2(2, Node(2))
+    assert dll == DoublyLinkedList([-1, 2, 0, 1])
+
+    dll.insertAtPosition2(2, dll.head)
+    assert dll == DoublyLinkedList([2, -1, 0, 1])
+
+    dll.insertAtPosition2(5, Node(3))
+    assert dll == DoublyLinkedList([2, -1, 0, 1, 3])
+
+# removeNodesWithValue
+def test_removeNodesWithValue():
+    dll = DoublyLinkedList([0, 1, 1, 2, 1, 3])
+
+    dll.removeNodesWithValue(-1)
+    assert dll == DoublyLinkedList([0, 1, 1, 2, 1, 3])
+
+    dll.removeNodesWithValue(1)
+    assert dll == DoublyLinkedList([0, 2, 3])
+
+    dll.removeNodesWithValue(3)
+    assert dll == DoublyLinkedList([0, 2])
+
+    dll.removeNodesWithValue(0)
+    assert dll == DoublyLinkedList([2])
+
+    dll.removeNodesWithValue(2)
+    assert dll == DoublyLinkedList([])
+
+    dll.removeNodesWithValue(0)
+    assert dll == DoublyLinkedList([])
+
+# remove
+def test_remove_middle():
+    dll = DoublyLinkedList([0, 1, 2, 3, 4])
+    middle = dll.head.next.next # value == 2
+
+    dll.remove(middle)
+
+    assert dll == DoublyLinkedList([0, 1, 3, 4])
+
+def test_remove_head():
+    dll = DoublyLinkedList([0, 1, 2])
+    dll.remove(dll.head)
+
+    assert dll == DoublyLinkedList([1, 2])
+
+def test_remove_tail():
+    dll = DoublyLinkedList([0, 1, 2])
+    dll.remove(dll.tail)
+
+    assert dll == DoublyLinkedList([0, 1])
+
+def test_remove_head_next():
+    dll = DoublyLinkedList([0, 1, 2, 3])
+    dll.remove(dll.head.next)
+
+    assert dll == DoublyLinkedList([0, 2, 3])
+
+def test_remove_tail_prev():
+    dll = DoublyLinkedList([0, 1, 2, 3])
+    dll.remove(dll.tail.prev)
+
+    assert dll == DoublyLinkedList([0, 1, 3])
+
+def test_remove_head_two_nodes():
+    dll = DoublyLinkedList([0, 1])
+    dll.remove(dll.head)
+
+    assert dll.head is dll.tail
+    assert dll == DoublyLinkedList([1])
+
+def test_remove_tail_two_nodes():
+    dll = DoublyLinkedList([0, 1])
+    dll.remove(dll.tail)
+
+    assert dll.head is dll.tail
+    assert dll == DoublyLinkedList([0])
+
+def test_remove_only_node():
+    dll = DoublyLinkedList([0])
+    dll.remove(dll.head)
+
+    assert dll == DoublyLinkedList([])
+
+# containsNodeWithValue
+def test_containsNodeWithValue():
+    dll = DoublyLinkedList([0, 1, 2])
+
+    assert dll.containsNodeWithValue(0)
+    assert dll.containsNodeWithValue(1)
+    assert dll.containsNodeWithValue(2)
+
+    assert not dll.containsNodeWithValue(3)
+
+def test_1():
+    dll = DoublyLinkedList([1, 2, 3, 4, 5])
+    
+    node1 = dll.head
+    node2 = node1.next
+    node3 = node2.next
+    node4 = node3.next
+    node5 = node4.next
+
+    node3A, node3B, node6 = Node(3), Node(3), Node(6)
+
+    assert dll == DoublyLinkedList([1, 2, 3, 4, 5])
+
+    dll.setHead(node4)
+    assert dll == DoublyLinkedList([4, 1, 2, 3, 5])
+
+    dll.setTail(node6)
+    assert dll == DoublyLinkedList([4, 1, 2, 3, 5, 6])
+
+    dll.insertBefore(node6, node3)
+    assert dll == DoublyLinkedList([4, 1, 2, 5, 3, 6])
+
+    dll.insertAfter(node6, node3)
+    assert dll == DoublyLinkedList([4, 1, 2, 5, 6, 3])
+
+def test_2():
+    node1 = Node(1)
+    node2 = Node(2)
+
+    node3 = Node(3)
+    node32 = Node(3)
+    node33 = Node(3)
+
+    node4 = Node(4)
+    node5 = Node(5)
+    node6 = Node(6)
+
+    dll = DoublyLinkedList()
+
+    dll.setHead(node5)
+    assert dll == DoublyLinkedList([5])
+
+    dll.setHead(node4)
+    assert dll == DoublyLinkedList([4, 5])
+
+    dll.setHead(node3)
+    assert dll == DoublyLinkedList([3, 4, 5])
+
+    dll.setHead(node2)
+    assert dll == DoublyLinkedList([2, 3, 4, 5])
+
+    dll.setHead(node1)
+    assert dll == DoublyLinkedList([1, 2, 3, 4, 5])
+
+    dll.setHead(node4)
+    assert dll == DoublyLinkedList([4, 1, 2, 3, 5])
+
+    dll.setTail(node6)
+    assert dll == DoublyLinkedList([4, 1, 2, 3, 5, 6])
+
+    dll.insertBefore(node6, node3)
+    assert dll == DoublyLinkedList([4, 1, 2, 5, 3, 6])
+
+    dll.insertAfter(node6, node32)
+    assert dll == DoublyLinkedList([4, 1, 2, 5, 3, 6, 3])
+
+    dll.insertAtPosition(1, node33)
+    assert dll == DoublyLinkedList([3, 4, 1, 2, 5, 3, 6, 3])
+
+    dll.removeNodesWithValue(3)
+    assert dll == DoublyLinkedList([4, 1, 2, 5, 6])
+    
+    dll.remove(node2)
+    assert dll == DoublyLinkedList([4, 1, 5, 6])
+
+    assert dll.containsNodeWithValue(5)
+
+
+
+
+
+
+
+
+
+
+
+
