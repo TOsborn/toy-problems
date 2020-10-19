53bc0355faa561788020454a8ccaf214fae805e0
Trenton Osborn <os.trenton@gmail.com>
2020-10-19 02:31:59 -0700

    move setup to fixture

--- algoexpert/bst_traversal/test_bst_traversal.py
+++ algoexpert/bst_traversal/test_bst_traversal.py
@@ -1,8 +1,9 @@
 from bst_traversal import BST, inOrderTraverse, preOrderTraverse, \
     postOrderTraverse
+import pytest
 
-
-def setup():
+@pytest.fixture
+def bstAndArray():
     one = BST(1)
     two = BST(2)
     three = BST(3)
@@ -21,22 +22,22 @@ def setup():
 
     return tree, array
 
-def test_inOrderTraverse():
-    tree, array = setup()
+def test_inOrderTraverse(bstAndArray):
+    tree, array = bstAndArray
     inOrderTraverse(tree, array)
 
     assert array == [4, 2, 5, 1, 3]
 
 
-def test_preOrderTraverse():
-    tree, array = setup()
+def test_preOrderTraverse(bstAndArray):
+    tree, array = bstAndArray
     preOrderTraverse(tree, array)
 
     assert array == [1, 2, 4, 5, 3]
 
 
-def test_postOrderTraverse():
-    tree, array = setup()
+def test_postOrderTraverse(bstAndArray):
+    tree, array = bstAndArray
     postOrderTraverse(tree, array)
 
     assert array == [4, 5, 2, 3, 1]
