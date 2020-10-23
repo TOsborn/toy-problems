25599ff75bf68ce220d19591d47d418ec734bc92
Trenton Osborn <os.trenton@gmail.com>
2020-10-23 15:55:28 -0700

    solve levenshtein distance

--- algoexpert/levenshtein_distance/levenshtein_distance.py
+++ algoexpert/levenshtein_distance/levenshtein_distance.py
@@ -1,5 +1,55 @@
 def levenshteinDistance(str1, str2):
     """Find the edit distance between two strings."""
+    str2, str1 = sorted((str1, str2), key=len)
+    # After ith step, prefix_distance[j] represents the number of edits
+    # required to transform str1[:i] into str2[:j]
+    prefix_distance = list(range(len(str2)+1))
+    for i in range(1, len(str1)+1):
+        next_prefix_distance = [i] + len(str2)*[None]
+        for j in range(1, len(str2)+1):
+            next_prefix_distance[j] = min(
+                prefix_distance[j] + 1,  # insertion
+                next_prefix_distance[j-1] + 1, # deletion
+                prefix_distance[j-1] + (str1[i-1] != str2[j-1]) # replacement
+            )
+
+        prefix_distance = next_prefix_distance
+
+    return prefix_distance[-1]
+
+################################
+
+def memoize(func):
+    """Memoize a function."""
+    memo = {}
+    def wrapper(*args):
+        if args not in memo:
+            memo[args] = func(*args)
+        
+        return memo[args]
+
+    return wrapper
+
+@memoize
+def levenshteinDistance1(str1, str2):
+    """Find the edit distance between two strings.
+    
+    A memoized recursive implementation.
+    """
+    if '' in (str1, str2):
+        return len(str1) or len(str2)
+
+    return min(
+        levenshteinDistance(str1, str2[:-1]) + 1,
+        levenshteinDistance(str1[:-1], str2) + 1,
+        levenshteinDistance(str1[:-1], str2[:-1]) + (str1[-1] != str2[-1])
+    )
+
+
+##############################
+
+def levenshteinDistance2(str1, str2):
+    """Find the edit distance between two strings."""
     str2, str1 = sorted([str1, str2], key=len)
     # After ith step, jth entry of prefix_dist is distance between
     # str2[:j] and str1[:i]. It is initialized to contain the edit
@@ -18,6 +68,7 @@ def levenshteinDistance(str1, str2):
             if last_c_appearance is not None:
                 new_prefix_dist[split_point] = min(
                     new_prefix_dist[split_point],
+                    # pylint: disable=invalid-sequence-index
                     prefix_dist[last_c_appearance] + split_point - last_c_appearance - 1
                 )
 
@@ -30,19 +81,8 @@ def levenshteinDistance(str1, str2):
 
 #################
 
-def memoize(func):
-    """Memoize a function."""
-    memo = {}
-    def wrapper(*args):
-        if args not in memo:
-            memo[args] = func(*args)
-        
-        return memo[args]
-
-    return wrapper
-
 @memoize
-def levenshteinDistance2(str1, str2):
+def levenshteinDistance3(str1, str2):
     """Find the edit distance between two strings."""
     if '' in (str1, str2):
         return _empty_string_dist(str1, str2)
